2dbcf69b4b1d7fa7c0749a965aff5cc7e2f26cda
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2024-05-18 14:21:12 +0300

    Change the theme

--- build-site.el
+++ build-site.el
@@ -20,7 +20,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://raw.githubusercontent.com/DiegoVicen/org-css/master/stylesheet.css\" />")
+      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"http://dakrone.github.io/org.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
