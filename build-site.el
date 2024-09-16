997c5ffc426f884b7583f9f566e385018b383b43
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2024-09-16 21:11:31 +0300

    Change MIME to plain/text

--- build-site.el
+++ build-site.el
@@ -20,7 +20,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"https://git.sr.ht/~khaled_mustafa/my-site/blob/master/css/org.css\" />")
+      org-html-head "<link rel=\"stylesheet\" type=\"text/plain\" href=\"https://git.sr.ht/~khaled_mustafa/my-site/blob/master/css/org.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
