06517c317d837f76ccde8074bc6a5bae44c457d1
Khaled Mustafa <khaled.mustafa.elsayed@outlook.com>
2024-09-16 23:14:55 +0300

    Changed theme directory

--- build-site.el
+++ build-site.el
@@ -20,7 +20,7 @@
 (setq org-html-validation-link nil            ;; Don't show validation link
       org-html-head-include-scripts nil       ;; Use our own scripts
       org-html-head-include-default-style nil ;; Use our own styles
-      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"../css/org.css\" />")
+      org-html-head "<link rel=\"stylesheet\" type=\"text/css\" href=\"styles/org.css\" />")
 
 ;; Define the publishing project
 (setq org-publish-project-alist
